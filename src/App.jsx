b890d88b7d3178c1c95e7ebdac47f014718ab972
Pushkar Sharma <ps100016@google.com>
2025-06-29 07:41:41 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="text-5xl min-h-screen text-center items-center">
+      BookLume
+    </div>
+  );
+};
+
+export default App;
